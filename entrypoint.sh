1a9240b5ef32d910c03520f3e0177dd601e3f421
mail2tor <qq@mail2tor.com>
2016-10-12 14:59:54 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
-/usr/local/bin/ssserver "$@"
+/usr/local/bin/ssserver  -p $SERVER_PORT \
+                         -k $PASSWORD \
+                         -m $METHOD \
+                         --user nobody 
+                         -d start
+
